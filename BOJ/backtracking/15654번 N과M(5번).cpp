745531fa16932b125b2e46faaabe80debea81a62
jaehyun9520 <64720923+jaehyun9520@users.noreply.github.com>
2020-05-09 14:32:24 +0900

    BOJ

---
+++ BOJ/backtracking/15654번 N과M(5번).cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+vector <int> list;
+int check[9];
+int ans[9];
+int n,m;
+		
+	void DFS(int cnt)
+	{int i;
+		if(cnt==m)
+		{
+			for(i=1; i<=m; i++)
+			{
+				printf("%d ",ans[i]);
+				
+		    }
+		printf("\n");
+		}
+		
+		else
+		{
+			for(i=0; i<n; i++)
+			{
+				if(check[i]==0)
+				{
+					ans[++cnt]=list[i];
+					check[i]=1;
+					DFS(cnt);
+					check[i]=0;
+					cnt--;
+				}
+			}
+		}
+		
+	}
+		
+int main()
+{
+//freopen("input.txt", "rt", stdin);
+int i,a;
+scanf("%d %d",&n,&m);
+
+ 
+ for(i=1; i<=n; i++)
+ {
+ 	scanf("%d",&a);
+ 	list.push_back(a);
+ }
+ 
+ sort(list.begin(),list.end());
+
+
+DFS(0);
+return 0;
+}
